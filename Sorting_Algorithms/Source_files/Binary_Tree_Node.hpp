2db3b038b101bb2cf8317db748370953d6ba2fbb
JonMS95 <jon_portugalete@hotmail.com>
2024-01-27 13:59:32 +0100

    Removed every message from BTNode constructor/destructor.

--- Sorting_Algorithms/Source_files/Binary_Tree_Node.hpp
+++ Sorting_Algorithms/Source_files/Binary_Tree_Node.hpp
@@ -11,25 +11,10 @@ public:
     std::shared_ptr<BTNode> left;
     std::shared_ptr<BTNode> right;
 
-    BTNode(void): data(0), left(nullptr), right(nullptr)
-    { 
-        std::cout << "Created node with no parameters." << std::endl;
-    }
-    
-    BTNode(T data): data(data), left(nullptr), right(nullptr)
-    {
-        std::cout << "Created node with data: " << this->data << "." << std::endl;
-    }
-
-    BTNode(T data, std::shared_ptr<BTNode> left, std::shared_ptr<BTNode> right): data(data), left(left), right(right)
-    {
-        std::cout << "Created node with data: " << this->data << " and populated pending nodes." << std::endl;
-    }
-
-    ~BTNode(void)
-    {
-        std::cout << "Destroyed node with data: " << this->data << "." << std::endl;
-    }
+    BTNode(void): data(0), left(nullptr), right(nullptr) {}
+    BTNode(T data): data(data), left(nullptr), right(nullptr) {}
+    BTNode(T data, std::shared_ptr<BTNode> left, std::shared_ptr<BTNode> right): data(data), left(left), right(right) {}
+    virtual ~BTNode(void) {}
 };
 
 #endif
