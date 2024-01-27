53d8f12199c363e856cbedc926b10704cd4ed6e9
JonMS95 <jon_portugalete@hotmail.com>
2024-01-27 15:19:08 +0100

    Constructor methods which use input parameters do take them as constants from now on.

--- Sorting_Algorithms/Source_files/Binary_Tree_Node.hpp
+++ Sorting_Algorithms/Source_files/Binary_Tree_Node.hpp
@@ -12,8 +12,8 @@ public:
     std::shared_ptr<BTNode> right;
 
     BTNode(void): data(0), left(nullptr), right(nullptr) {}
-    BTNode(T data): data(data), left(nullptr), right(nullptr) {}
-    BTNode(T data, std::shared_ptr<BTNode> left, std::shared_ptr<BTNode> right): data(data), left(left), right(right) {}
+    BTNode(const T data): data(data), left(nullptr), right(nullptr) {}
+    BTNode(const T data, const std::shared_ptr<BTNode> left, const std::shared_ptr<BTNode> right): data(data), left(left), right(right) {}
     virtual ~BTNode(void) {}
 };
 
