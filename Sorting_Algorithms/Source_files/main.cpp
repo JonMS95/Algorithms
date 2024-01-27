be26f20dff8721c528d711378d4149246da35e1e
JonMS95 <jon_portugalete@hotmail.com>
2024-01-27 13:57:08 +0100

    SortTest now does return a boolean, so the overall result of every test for each algorithm can now be easily retrieved.

--- Sorting_Algorithms/Source_files/main.cpp
+++ Sorting_Algorithms/Source_files/main.cpp
@@ -1,12 +1,25 @@
 #include "Sorting_Algorithms.hpp"
-#include "Binary_Search_Tree.hpp"
 
 int main()
 {
-    int number_of_vectors = 5;
-    int vector_size = 10;
-    SortingAlgorithms::SortTest<int>(SortingAlgorithms::BubbleSort              , number_of_vectors ,   vector_size);
-    SortingAlgorithms::SortTest<int>(SortingAlgorithms::InsertionSort           , number_of_vectors ,   vector_size);
-    SortingAlgorithms::SortTest<int>(SortingAlgorithms::SelectionSort           , number_of_vectors ,   vector_size);
-    SortingAlgorithms::SortTest<int>(SortingAlgorithms::BinarySearchTreeSort    , number_of_vectors ,   vector_size);
+    unsigned int number_of_vectors  = 10    ;
+    unsigned int vector_size        = 100   ;
+
+    std::vector<bool> results;
+
+    results.emplace_back(   SortingAlgorithms::SortTest<int>(SortingAlgorithms::BubbleSort              , number_of_vectors ,   vector_size)    );
+    results.emplace_back(   SortingAlgorithms::SortTest<int>(SortingAlgorithms::InsertionSort           , number_of_vectors ,   vector_size)    );
+    results.emplace_back(   SortingAlgorithms::SortTest<int>(SortingAlgorithms::SelectionSort           , number_of_vectors ,   vector_size)    );
+    results.emplace_back(   SortingAlgorithms::SortTest<int>(SortingAlgorithms::BinarySearchTreeSort    , number_of_vectors ,   vector_size)    );
+
+    for(auto i = 0; i < results.size(); i++)
+        if(!results[i])
+        {
+            std::cout << "TEST " << i << " FAILED!" << std::endl;
+            return -1;
+        }
+
+    std::cout << "ALL TESTS ARE OK." << std::endl;
+
+    return 0;
 }
