1d658ec972cfe29aad03455694529620719862cb
JonMS95 <jon_portugalete@hotmail.com>
2024-01-28 18:04:37 +0100

    Added QuickSort, incremented number of tests required for each algorithm.

--- Sorting_Algorithms/Source_files/main.cpp
+++ Sorting_Algorithms/Source_files/main.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-    unsigned int number_of_vectors  = 10    ;
-    unsigned int vector_size        = 100   ;
+    unsigned int number_of_vectors  = 100   ;
+    unsigned int vector_size        = 1000  ;
 
     std::vector<bool> results;
 
@@ -11,6 +11,7 @@ int main()
     results.emplace_back(   SortingAlgorithms::SortTest<int>(SortingAlgorithms::InsertionSort           , number_of_vectors ,   vector_size)    );
     results.emplace_back(   SortingAlgorithms::SortTest<int>(SortingAlgorithms::SelectionSort           , number_of_vectors ,   vector_size)    );
     results.emplace_back(   SortingAlgorithms::SortTest<int>(SortingAlgorithms::BinarySearchTreeSort    , number_of_vectors ,   vector_size)    );
+    results.emplace_back(   SortingAlgorithms::SortTest<int>(SortingAlgorithms::QuickSort               , number_of_vectors ,   vector_size)    );
 
     for(auto i = 0; i < results.size(); i++)
         if(!results[i])
