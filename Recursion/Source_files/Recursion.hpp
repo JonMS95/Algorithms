636e8ce3f2ba792403d82b5403c91e03f2eb8526
JonMS95 <jon_portugalete@hotmail.com>
2024-02-24 13:55:14 +0100

    Created a class that contains just recursive functions such as summation and factorial.

---
+++ Recursion/Source_files/Recursion.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Recursion
+{
+public:
+    Recursion()     = delete;
+    ~Recursion()    = delete;
+
+    static unsigned long int summation(unsigned long int n)         ;
+    static unsigned long int summationIterative(unsigned long int n);
+    static unsigned long int factorial(unsigned long int n)         ;
+    static unsigned long int factorialIterative(unsigned long int n);
+    static unsigned long int fibonacci(unsigned long int n)         ;
+    static unsigned long int fibonacciIterative(unsigned long int n);
+    
+    static bool testRecursiveFunction(  unsigned long int(*recursiveFunction)(unsigned long int),
+                                        unsigned long int(*iterativeFunction)(unsigned long int),
+                                        unsigned int n                                          ,
+                                        std::string function_name                               );
+};
