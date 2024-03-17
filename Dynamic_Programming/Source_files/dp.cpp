8fd1e134ae090061a94797a1339c9f3270f65165
JonMS95 <jon_portugalete@hotmail.com>
2024-03-17 18:43:52 +0100

    Added another example for memoization. Ot should be modified a little bit so it makes more sense to use DP. Tabulation explanation for this case is required as well.

--- Dynamic_Programming/Source_files/dp.cpp
+++ Dynamic_Programming/Source_files/dp.cpp
@@ -40,6 +40,10 @@ S(2) = 2 + S(1)
 S(1) = 1
 
 And as shown above, each calculation has to be performed just once, so there is no need to store those results each time.
+
+Anyway, those can be easily stored as well in a variable that may be outside our function scope, so that there's no need to calculate it
+again if it has already been done so. For example, if we have calculated first 5!, we need to calculate the factorial for any lower number.
+If we wanted to calculate 3!, there would be no need to perform any mathematical operation again as it would have been already done.
 */
 
 #include "dp.hpp"
@@ -84,4 +88,130 @@ unsigned int DP::fibonacci_memoization(const unsigned int n)
     fib_nums[1] = 1;    
 
     return fibonacci_memoization_helper(n, fib_nums);
+}
+
+/*
+Let's see another example: given an integer n, return a vector which contains the amount of bits for every number from 0 to n.
+Examples:
+    n = 2 -> [0, 1, 1]
+    n = 5 -> [0, 1, 1, 2, 1, 2]
+Source: https://leetcode.com/problems/counting-bits/
+*/
+
+/*
+Naïve approach: calculate first the size of the integer. Then, for i = 0; i < size_of_int; i++, divide it by two (or apply single bit mask AND),
+and add 1 each time a one is found in the current number's binary representation.
+Cost: O(n) * O(K) where K is equal to the number of bits in an integer (typically 32).
+*/
+
+std::vector<int> DP::countBits_naive(int n)
+{
+    std::vector<int> ret(n + 1);
+    
+    for(int i = 0; i <= n; i++)
+    {
+        int num_to_divide = i;
+        int count_ones_curr_num = 0;
+        
+        for(int j = 0; j < (int)(sizeof(n) * 8); j++)
+        {
+            if(num_to_divide % 2 == 1)
+                ++count_ones_curr_num;
+            num_to_divide /= 2;
+        }
+
+        ret[i] = count_ones_curr_num;
+    }
+
+    return ret;
+}
+
+/*
+Recursive approach: realise the pattern.
+N(0)    = 0     
+N(1)    = 1     = N(0) + 1
+N(2)    = 1     = N(0) + 1
+N(3)    = 2     = N(1) + 1  = N(0) + 1 + 1
+N(4)    = 1     = N(0) + 1
+N(5)    = 2     = N(1) + 1  = N(0) + 1
+N(6)    = 2     = N(2) + 1  = N(0) + 1 + 1
+N(7)    = 3     = N(3) + 1  = N(1) + 1 + 1  = N(0) + 1 + 1 + 1
+N(8)    = 1     = N(0) + 1
+N(9)    = 2     = N(1) + 1  = N(0) + 1 + 1
+N(10)   = 2     = N(2) + 1  = N(0) + 1 + 1
+N(11)   = 3     = N(3) + 1  = N(1) + 1      = N(0) + 1 + 1
+N(12)   = 2     = N(4) + 1  = N(0) + 1 + 1
+N(13)   = 3     = N(5) + 1  = N(1) + 1      = N(0) + 1 + 1
+N(14)   = 3     = N(6) + 1  = N(2) + 1 + 1  = N(0) + 1 + 1 + 1
+N(15)   = 4     = N(7) + 1  = N(3) + 1 + 1  = N(1) + 1 + 1 + 1 = N(0) + 1 + 1 + 1 + 1
+N(16)   = 1     = N(0) + 1
+...
+
+Therefore, we can infere that:
+N(n)    = 0 if n == 0
+        = N(n - p) + 1 if n > 0, where p is the nearest lower power of two
+
+For example, for N(5), p = 4 (2 ^ 2), thus N(5) = N(5 - 4) + 1 = N(1) + 1.
+In the case of N(1), p = 1 (2 ^ 0), which leaves us with N(1) = N(1 - 1) + 1 = N(0) + 1 = 1
+So finally, N(5) = N(0) + 1 + 1
+
+As a consequence of having to find p, an auxiliary function should be designed to fulfill the mentioned purpose.
+*/
+
+int DP::getLE2Power(int n)
+{
+    if(n <= 0)
+        return -1;
+    
+    for(int i = 0; i < (int)(sizeof(n) * 8); i++)
+        if( (n >= (1 << i) ) && (n < (1 << (i + 1) ) ) )
+            return (1 << i);
+    
+    return -1;
+}
+
+int DP::getNumOfOnes_recursive(int n)
+{
+    if(n == 0)
+        return 0;
+    
+    return (DP::getNumOfOnes_recursive(n - DP::getLE2Power(n)) + 1);
+}
+
+std::vector<int> DP::countBits_recursive(int n)
+{
+    std::vector<int> ret(n + 1);
+
+    for(int i = n; i >= 0; i--)
+        ret[i] = DP::getNumOfOnes_recursive(i);
+    
+    return ret;
+}
+
+/*
+Once the problem has been solved by using an iterative approach, it's time to avoid redundant calculations.
+For instance, if we were to calculate N(15), there would be no need to calculate any other value, since those have already been
+calculated previously. This can be easily solved by using the array in which all of the results are stored as a look up table.
+*/
+
+int DP::getNumOfOnes_memoization(int n, std::vector<int>& known_values)
+{
+    if(n == 0)
+        return 0;
+    
+    int required_arg = n - DP::getLE2Power(n);
+    if(known_values[required_arg] == -1)
+        known_values[required_arg] = DP::getNumOfOnes_memoization(required_arg, known_values);
+
+    return (known_values[required_arg] + 1);
+}
+
+std::vector<int> DP::countBits_memoization(int n)
+{
+    std::vector<int> ret(n + 1, -1);
+
+    for(int i = n; i >= 0; i--)
+        ret[i] = DP::getNumOfOnes_memoization(i, ret);
+    
+    return ret;
 }
