24ff63477a94b276c6dbdac6155e1b7c8a1cc468
JonMS95 <jon_portugalete@hotmail.com>
2024-03-17 18:44:47 +0100

    Added naïve, recursive, and memoization function protoypes for countBits exercise (Leetcode  338).

--- Dynamic_Programming/Source_files/dp.hpp
+++ Dynamic_Programming/Source_files/dp.hpp
@@ -8,9 +8,17 @@ class DP
 private:
     static unsigned int fibonacci_memoization_helper(const unsigned int n, std::vector<int>& fib_nums);
 
+    static int getLE2Power(int n);
+    static int getNumOfOnes_recursive(int n);
+
 public:
     static unsigned int fibonacci_tabulation(const unsigned int n);
     static unsigned int fibonacci_memoization(const unsigned int n);
+
+    static std::vector<int> countBits_naive(int n);
+    static std::vector<int> countBits_recursive(int n);
+    static int getNumOfOnes_memoization(int n, std::vector<int>& known_values);
+    static std::vector<int> countBits_memoization(int n);
 };
 
 #endif
